a83cc5bbe7085545108fcc1e4cc198c9039fdf7f
Ricozero <735784247@qq.com>
2019-09-15 11:01:55 +0800

    完成打开数据集

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <Workspace/UGWorkspace.h>
 using namespace UGC;
 
@@ -10,9 +11,41 @@ int main()
 		cout << "工作空间打开失败！" << endl;
 		return -1;
 	}
+	cout << "工作空间共包含：" << endl;
+	cout << workspace.m_MapStorages.GetCount() << "张地图" << endl;
+	cout << workspace.m_DataSources.GetCount() << "个数据源" << endl << endl;
 
-	cout << "工作空间共包含" << workspace.m_MapStorages.GetCount() << "张地图" << endl;
+	UGDataSource* dataSource = workspace.GetDataSource(0);
+	if (!dataSource->Open())
+	{
+		cout << "数据源打开失败！" << endl;
+		return -1;
+	}
+	cout << "打开第一个数据源，共包含：" << endl;
+	cout << dataSource->GetDatasetCount() << "个数据集" << endl << endl;
+
+	UGDatasets* dataSets = dataSource->GetDatasets();
+	UGDataset** dataSetsP = dataSets->GetData();
+	UGDataset* dataSet;
+	int i;
+	cout << "各个数据集信息：" << endl;
+	for (i = 0; i < dataSource->GetDatasetCount(); i++)
+	{
+		dataSet = dataSetsP[i];
+		wcout << i << "：类型为" << dataSet->GetType()
+			<< "，名称为" << dataSet->GetName().Cstr()
+			<< "，表名为" << dataSet->GetTableName().Cstr()
+			<< endl;
+	}
+
+	i = 6;
+	dataSet = dataSetsP[i];
+	if (!dataSet->Open())
+	{
+		cout << "数据集打开失败！" << endl;
+		return -1;
+	}
+	cout << "打开索引为" << i << "的数据集" << (dataSet->IsVector() ? "是" : "不是") << "矢量" << endl;
 
 	return 0;
-
 }
