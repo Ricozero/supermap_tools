6c9ce9bb8fb9ad32580264969cd176705abd5193
Ricozero <735784247@qq.com>
2019-10-13 00:07:47 +0800

    完成json数据到vector，再到超图面数据的转换

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <time.h>
 #include <Workspace/UGWorkspace.h>
 #include <Geometry/UGGeometry.h>
 #include "json/CJsonObject.hpp"
 using namespace UGC;
 
-int testSuperMap()
+void testSuperMap()
 {
 	UGWorkspace workspace;
 	UGString workspacePath = _U("D:\\Program Files\\SuperMap iDesktop 9D\\SampleData\\China\\China100\\China.smwu");
 	if (!workspace.Open(workspacePath))
 	{
-		cout << "工作空间打开失败！" << endl;
-		return -1;
+		cout << "工作空间打开失败，可能被其他程序占用！" << endl;
 	}
 	cout << "工作空间共包含：" << endl;
 	cout << workspace.m_DataSources.GetCount() << "个数据源" << endl;
@@ -23,8 +23,7 @@ int testSuperMap()
 	UGDataSource *dataSource = workspace.GetDataSource(0);
 	if (!dataSource->Open())
 	{
-		cout << "数据源打开失败！" << endl;
-		return -1;
+		cout << "数据源打开失败，可能被其他程序占用！" << endl;
 	}
 	cout << "打开第一个数据源，共包含：" << endl;
 	cout << dataSource->GetDatasetCount() << "个数据集" << endl;
@@ -49,7 +48,6 @@ int testSuperMap()
 	if (!dataSet->Open())
 	{
 		cout << "数据集打开失败！" << endl;
-		return -1;
 	}
 	cout << "打开索引为" << i << "的数据集（" << (dataSet->IsVector() ? "是" : "不是") << "矢量）" << endl;
 
@@ -81,15 +79,20 @@ int testSuperMap()
 	for (i = 0; i < pRegion->GetSubPntCount(0); i++)
 		cout << '(' << pPoint2D[i].x << ", " << pPoint2D[i].y << ')' << endl;
 
+	// 打开数据集后生成一个一级内存副本
+	// 查询得到记录集后又会生成一个二级内存副本
+	// 得到的指针也是指向二级副本的
+	// 一级副本在对应的记录集调用Edit之后才可以被修改
+	// 调用SetGeometry以修改一级副本
+	// 调用Update将一级副本写回文件
 	recordset->Edit();
 	recordset->SetGeometry(*pRegion);
 	cout << "更新记录集，结果为：" << (recordset->Update() ? "成功" : "失败") << endl;
 
 	delete pGeometry;
-	return 0;
 }
 
-int testJson()
+void testJson()
 {
 	int iValue;
 	double fTimeout;
@@ -176,24 +179,146 @@ int testJson()
 	std::cout << oJson.IsNull("test_float") << "\t" << oJson.IsNull("null_value") << std::endl;
 	oJson["test_float"].AddNull();
 	std::cout << oJson.ToString() << std::endl;
+}
+
+void testPolyJson()
+{
+	ifstream fin("D:\\_save\\satellite_seg\\results\\1_poly_1.json");
+	if (!fin.is_open())
+	{
+		cout << "文件打开失败！" << endl;
+	}
+	stringstream buffer;
+	buffer << fin.rdbuf();
+	string text(buffer.str());
+
+	neb::CJsonObject oJson;
+	oJson.Parse(text);
+	//cout << oJson.ToFormattedString();
+	cout << oJson.IsArray() << endl;
+	cout << oJson.GetArraySize() << endl;
+	cout << oJson[0].IsArray() << endl;
+	cout << oJson[0].GetArraySize() << endl;
+	cout << oJson[0][0].IsArray() << endl;
+	cout << oJson[0][0].GetArraySize() << endl;
+	float x, y;
+	oJson[0][0].Get(0, x);
+	oJson[0][0].Get(1, y);
+	cout << x << ' ' << y << endl;
+
+	// CJsonObject的Array是用链表实现，遍历速度慢
+	int i, j;
+	vector<vector<vector<float>>> plist;
+
+	clock_t t = clock();
+	/***** 计算部分 *****/
+	for (i = 0; i < oJson.GetArraySize(); i++)
+	{
+		plist.push_back({});
+		for (j = 0; j < oJson[i].GetArraySize(); j++)
+		{
+			plist[i].push_back({});
+			oJson[i][j].Get(0, x);
+			oJson[i][j].Get(1, y);
+			plist[i][j].push_back(x);
+			plist[i][j].push_back(y);
+		}
+	}
+	/********************/
+	cout << "Json遍历耗时" << (clock() - t) * 1.0 / CLOCKS_PER_SEC << 's' << endl; // 109s, 79s
+
+	t = clock();
+	/***** 计算部分 *****/
+	for (i = 0; i < plist.size(); i++)
+	{
+		for (j = 0; j < plist[i].size(); j++)
+		{
+			x = plist[i][j][0];
+			y = plist[i][j][1];
+		}
+	}
+	/********************/
+	cout << "vector遍历耗时" << (clock() - t) * 1.0 / CLOCKS_PER_SEC << 's' << endl; // <0.1s
+}
+
+int Json2Vector(const string filename, vector<vector<vector<float>>> &plist)
+{
+	ifstream fin(filename);
+	if (!fin.is_open())
+		return -1;
+
+	stringstream buffer;
+	buffer << fin.rdbuf();
+	string text(buffer.str());
+
+	neb::CJsonObject oJson;
+	oJson.Parse(text);
+
+	// TODO: 此处应检查格式
+	float x, y;
+	int i, j;
+	for (i = 0; i < oJson.GetArraySize(); i++)
+	{
+		plist.push_back({});
+		for (j = 0; j < oJson[i].GetArraySize(); j++)
+		{
+			plist[i].push_back({});
+			oJson[i][j].Get(0, x);
+			oJson[i][j].Get(1, y);
+			plist[i][j].push_back(x);
+			plist[i][j].push_back(y);
+		}
+	}
 	return 0;
 }
 
-int testPolyJson()
+bool Vector2Region(const vector<vector<float>> &polygon, UGGeoRegion *region)
 {
-	ifstream in("D:\\_save\\satellite_seg\\results\\test_poly_1.json");
-	stringstream buffer;
-	buffer << in.rdbuf();
-	string text(buffer.str());
+	if (!region)
+		region = new UGGeoRegion;
 
-	neb::CJsonObject jo;
-	jo.Parse(text);
-	cout << jo.ToString();
-	return 0;
+	int i;
+	vector<UGPoint2D> points;
+	for (i = 0; i < polygon.size(); i++)
+		points.push_back({ polygon[i][0],polygon[i][1] });
+	UGGeoRegion *subRegion = NULL;
+	cout << (region->GetSub(0, subRegion)?1:0) << endl;
+	cout << region->GetSubCount() << endl;
+	return region->InsertSub(0, points.data(), points.size());// 抛出异常
+}
+
+void testV2R()
+{
+	vector<vector<vector<float>>> plist;
+	if (Json2Vector("D:\\_save\\satellite_seg\\results\\1_small_pred_poly_1.json", plist) < 0)
+		cout << "转换失败" << endl;
+	UGGeoRegion *region = NULL;
+	cout << Vector2Region(plist[0], region) << endl;
 }
 
 int main()
 {
-	testSuperMap();
+	int n = 0;
+	cout << "输入测试对象：";
+	while (n < 1 || n > 4)
+		cin >> n;
+	cin.clear();
+	cin.ignore();
+
+	switch (n)
+	{
+	case 1:
+		testSuperMap();
+		break;
+	case 2:
+		testJson();
+		break;
+	case 3:
+		testPolyJson();
+		break;
+	case 4:
+		testV2R();
+		break;
+	}
 	return 0;
 }
